1b4e27847ee89908f914b372ecf9fd7ba7e81c9f
guelli1 <guelli.work@gmail.com>
2023-08-25 18:14:35 -0700

    4-puts.c vi 4-puts.c
    betty 4-puts.c
    git commit -m 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  main - A program that prints a linen
+ *  main - A program that prints a line with puts function
  *  Return: 0 (Success)
  */
 int main(void)
 {
-puta("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
